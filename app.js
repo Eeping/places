43ba67bff1091ac5a616405c720af0ca6c17bedd
Eeping <eeping.96@gmail.com>
2018-06-07 17:57:20 +0800

    Added places API key

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ server.use(bodyParser.urlencoded({extended: true}));//check body form
 server.set('view engine', 'hbs'); //set server to use hbs as views engine
 hbs.registerPartials(__dirname + '/views/partials') //tell hbs that you use partials
 
+const PLACES_API_KEY = 'AIzaSyDDzo12RmOPO1X08qGD2P62TkxWxvqHdYc';
+
 //path to root directory
 server.get('/', (req, res) => {
   res.render('home.hbs');
@@ -17,6 +19,27 @@ server.get('/', (req, res) => {
 
 server.get('/form', (req, res) => {
   res.render('form.hbs');
+});
+
+server.post('/getplaces', (req, res) => {
+  const addr = req.body.address; //extract content of address from txtbox
+  const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyBo54sDKP0LPhJBhmX7FsUPBRZYsC4YczI`;
+
+  axios.get(locationReq).then((response) => {
+    //javascript object, key -> value
+    const locationData = {
+      addr: response.data.results[0].formatted_address,
+      lat: response.data.results[0].geometry.location.lat,
+      lng: response.data.results[0].geometry.location.lng,
+    }
+
+    const placesReq=`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500&types=food&name=food&key=${PLACES_API_KEY}`;
+
+    console.log(locationData);
+    res.status(200).send(locationData);
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 server.listen(port, () => {
