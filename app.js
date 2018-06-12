16ae4848bd0b061569d1d7bbdb23c978beaab0f0
Eeping <eeping.96@gmail.com>
2018-06-12 13:00:05 +0800

    Added result, table

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ server.set('view engine', 'hbs'); //set server to use hbs as views engine
 hbs.registerPartials(__dirname + '/views/partials') //tell hbs that you use partials
 
 const PLACES_API_KEY = 'AIzaSyDDzo12RmOPO1X08qGD2P62TkxWxvqHdYc';
+var filteredResults; //global variable
+
+hbs.registerHelper('list', (items, options) => {
+  items = filteredResults;
+  var out = "<tr><th>Name</th></tr>";
+  const length = items.length;
+
+  for (var i = 0; i < length; i++){
+    out = out + options.fn(items[i]);
+  }
+  return out;
+});
 
 //path to root directory
 server.get('/', (req, res) => {
@@ -23,6 +35,9 @@ server.get('/form', (req, res) => {
 
 server.post('/getplaces', (req, res) => {
   const addr = req.body.address; //extract content of address from txtbox
+  const placetype = req.body.placetype;
+  const name = req.body.name;
+
   const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyBo54sDKP0LPhJBhmX7FsUPBRZYsC4YczI`;
 
   axios.get(locationReq).then((response) => {
@@ -33,14 +48,35 @@ server.post('/getplaces', (req, res) => {
       lng: response.data.results[0].geometry.location.lng,
     }
 
-    const placesReq=`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500&types=food&name=food&key=${PLACES_API_KEY}`;
+    const placesReq=`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500&types=${placetype}&name=${name}&key=${PLACES_API_KEY}`;
+
     return axios.get(placesReq);
   }).then((response) =>{
-    res.status(200).send(response.data.results);
+
+    filteredResults = extractData(response.data.results); //extractData return an array
+
+    res.render('result.hbs');
+    //res.status(200).send(filteredResults);
   }).catch((error) => {
     console.log(error);
   });
 });
+
+const extractData = (originalResults) => {
+  var placesObj = {
+    table : [], //array to store filteredResults
+  };
+
+  const length = originalResults.length;
+
+  for(var i = 0; i < length; i++){
+    tempObj = {
+      name : originalResults[i].name,
+    }
+    placesObj.table.push(tempObj);
+  }
+  return placesObj.table;
+};
 
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
